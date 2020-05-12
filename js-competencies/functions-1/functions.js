95a6961b436206f86ffb6ce935968fb252269ef2
andre <andrelaprov@gmail.com>
2020-05-12 01:21:18 -0400

    more practice

--- js-competencies/functions-1/functions.js
+++ js-competencies/functions-1/functions.js
@@ -1,16 +1,16 @@
 //========= DO NOT TOUCH THIS CODE =========//
 try {
-  var a = hey
+  var a = hey;
 } catch (err) {
   var a;
 }
 try {
-  var b = hi
+  var b = hi;
 } catch (err) {
   var b;
 }
 try {
-  var c = hello
+  var c = hello;
 } catch (err) {
   var c;
 }
@@ -24,6 +24,9 @@ try {
 
 //Code here
 
+function hey(name) {
+  return `Hey ${name}`;
+}
 
 ///////////////////Problem 2//////////////////
 //Create a function called hi, using a function
@@ -32,6 +35,9 @@ try {
 
 //Code here
 
+let hi = function (name) {
+  return `Hi ${name}`;
+};
 
 ///////////////////Problem 3//////////////////
 //Create a function called hello, using an arrow
@@ -40,20 +46,22 @@ try {
 
 //Code here
 
+let hello = (name) => {
+  return `Hello ${name}`;
+};
 
 ///////////////////Problem 4//////////////////
 //Create a function named fun, which takes in
 //a single string parameter, and returns that string
-//with "!!!!!" added to the end of it. Then, call fun 
-//with the string "Yay" and save the result of the function to a 
+//with "!!!!!" added to the end of it. Then, call fun
+//with the string "Yay" and save the result of the function to a
 //variable called funTimes.
 
 //Code here
 
-
 ///////////////////Problem 5//////////////////
 //Finish the function 'climb' below. Use the built-in local
-//variable arguments, within the function climb. 
+//variable arguments, within the function climb.
 
 //If you do not know what the arguments variable is,
 //check out: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
@@ -62,7 +70,7 @@ try {
 //The purpose of this exercise is to use the local arguments
 //variable within the function scope.
 
-//If there is a string at arguments[0] but arguments[1] is falsy, return "On belay?". 
+//If there is a string at arguments[0] but arguments[1] is falsy, return "On belay?".
 
 //If there is a string at arguments[0], and true at arguments[1],
 //return "Climbing!"
@@ -73,9 +81,7 @@ function climb() {
   //What is the arguments variable? Try calling climb
   //and passing in ("Ellie", true) and see what is logged
   //to the console.
-  console.log('function climb\'s local arguments var - ', arguments)
+  console.log("function climb's local arguments var - ", arguments);
 
   //CODE HERE - DO NOT TOUCH THE CODE ABOVE!
-
 }
-
